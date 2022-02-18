f31f966a286005b99b228882fe848c4ae6bd4cfa
Aum1234 <lalida.am.62@ubu.ac.th>
2022-02-18 21:26:33 +0700

    Frist

---
+++ painaipaikan/src/component/config.js
@@ -0,0 +1,13 @@
+
+
+const config = {
+    apiKey: "AIzaSyB07TaJZmMrIWUMv7asX0O3y695jZBdEL4",
+    authDomain: "pai-nai-pai-kan-4eaee.firebaseapp.com",
+    databaseURL: "https://pai-nai-pai-kan-4eaee-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "pai-nai-pai-kan-4eaee",
+    storageBucket: "pai-nai-pai-kan-4eaee.appspot.com",
+    messagingSenderId: "893864262764",
+    appId: "1:893864262764:web:5cbbe9c5d0f2825c562c49"
+  };
+
+  export default config
